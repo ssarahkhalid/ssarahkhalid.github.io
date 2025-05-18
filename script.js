b692429dcf7ff4b8471f6c07acd3e453df372ada
ssarahkhalid <88570249+ssarahkhalid@users.noreply.github.com>
2025-05-18 19:08:31 -0400

    removing product folder files back to own individual html files

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
+  var icon = x.querySelector(".icon i");
   if (x.className === "topnav") {
     x.className += " responsive";
+    icon.className = "fa fa-times";
   } else {
     x.className = "topnav";
+    icon.className = "fa fa-bars";
   }
 }
