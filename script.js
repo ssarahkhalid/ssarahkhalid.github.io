7b4aeb1aa90e1b583303c38b8e5729d6d4e266a1
ssarahkhalid <sar.khalid@outlook.com>
2025-12-25 18:32:43 -0500

    Update navigation links, redesign landing pages, and add follow section

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
+  var icon = document.getElementById("menuIcon");
   if (x.className === "topnav") {
     x.className += " responsive";
+    // Change icon from bars to times (X)
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-times");
   } else {
     x.className = "topnav";
+    // Change icon from times (X) back to bars
+    icon.classList.remove("fa-times");
+    icon.classList.add("fa-bars");
   }
 }
